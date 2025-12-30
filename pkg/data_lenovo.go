b35caea3f9a57652bc00039015e0f07fb8118525
src-csm <199741225+src-csm@users.noreply.github.com>
2025-12-30 01:26:18 +0000

    fix(devicetype-library): updating data from remote repo (2025-12-30).

--- pkg/data_lenovo.go
+++ pkg/data_lenovo.go
@@ -371,6 +371,43 @@ var DeviceTypesMaplenovo = map[string]*DeviceData{
             { Name: "XCC", Label: "", Type: "1000base-t", MgmtOnly: true },
         },
     },
+    "ThinkCentre M75q Gen 5 Tiny": {
+        Manufacturer: "Lenovo",
+        Model: "ThinkCentre M75q Gen 5 Tiny",
+        Slug: "lenovo-thinkcentre-m75q-gen-5-tiny",
+        UHeight: 1,
+        PartNumber: "",
+        IsFullDepth: false,
+        Airflow: "top-to-bottom",
+        FrontImage: false,
+        RearImage: false,
+        SubdeviceRole: "",
+        Weight: 1.32,
+        WeightUnit: "",
+        IsPowered: false,
+        ConsolePorts: []ConsolePort{
+        },
+        ConsoleServerPorts: []ConsoleServerPort{
+        },
+        PowerPorts: []PowerPort{
+            { Name: "power1", Label: "", Type: "dc-terminal", MaximumDraw: 90, AllocatedDraw: 66 },
+        },
+        PowerOutlets: []PowerOutlet{
+        },
+        FrontPorts: []FrontPort{
+        },
+        RearPorts: []RearPort{
+        },
+        ModuleBays: []ModuleBay{
+        },
+			  DeviceBays: []DeviceBay{
+        },
+        InventoryItems: []InventoryItem{
+        },
+        Interfaces: []Interface{
+            { Name: "ether1", Label: "", Type: "1000base-t", MgmtOnly: false },
+        },
+    },
     "ThinkServer RD230": {
         Manufacturer: "Lenovo",
         Model: "ThinkServer RD230",
